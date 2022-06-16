bc370c505ffaefe2cc5062f8ff08d2b3f40e3c76
Your Name <you@example.com>
2022-06-16 22:49:13 +0600

    first node project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = {name,email};
+
+    // POST Data to the server
+    fetch("http://localhost:5000/user", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const newUsers = [...users, data];
+        setUsers(newUsers)
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>My own data = {users.length}</h3>
+      <form onSubmit={handleUser}>
+        <input type="text" name="name" placeholder="Name" />
+        <input
+          type="email"
+          name="email"
+          placeholder="
+      Email.."
+          id="email"
+        />
+        <input type="submit" value="Submit" />
+      </form>
+      <ol>
+        {
+          users.map(user => <li key={user.id}>Name : {user.name} email : {user.email}</li>)
+        }
+      </ol>
     </div>
   );
 }
